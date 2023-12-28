64137152d1474daa694298a009f3188e3721b149
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-12-28 23:08:06 +0100

    simplified code removing a component

--- src/components/BlockedSitesSection.jsx
+++ src/components/BlockedSitesSection.jsx
@@ -1,37 +1,81 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function BlockedSitesSection({
+  index,
   title,
   sites,
-  onAddSite,
-  onEditSite,
-  onDeleteSite,
+  isModalOpen,
+  onOpenModal,
+  onCloseModal,
+  onAddWebsite,
+  onEditWebsite,
+  onEditSectionTitle,
 }) {
+  const [newWebsite, setNewWebsite] = useState("");
+  const [editedTitle, setEditedTitle] = useState(title);
+
+  const handleAddWebsite = () => {
+    onAddWebsite(newWebsite);
+    setNewWebsite("");
+  };
+
+  const handleEditSectionTitle = () => {
+    onEditSectionTitle(editedTitle);
+  };
+
   return (
     <div className="blocker-sites-section">
-      <h2>{title}</h2>
-      {sites.length > 0 ? (
-        <ul>
-          {sites.map((site, index) => (
-            <li key={index}>
-              {site}
-              <button onClick={() => onEditSite(index)}>Edit</button>
-              <button onClick={() => onDeleteSite(index)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No Sites added yet</p>
+      <h2 onClick={onOpenModal}>{title}</h2>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <input
+              type="text"
+              value={newWebsite}
+              onChange={(e) => setNewWebsite(e.target.value)}
+              placeholder="Enter new website"
+            />
+            <button onClick={handleAddWebsite}>Add Website</button>
+            <input
+              type="text"
+              value={editedTitle}
+              onChange={(e) => setEditedTitle(e.target.value)}
+              placeholder="Edit section title"
+            />
+            <button onClick={handleEditSectionTitle}>Edit Section Title</button>
+            <button onClick={onCloseModal}>Close</button>
+          </div>
+        </div>
       )}
-      <button onClick={onAddSite}>Add Website</button>
+
+      <ul>
+        {sites.map((site, siteIndex) => (
+          <li key={siteIndex}>
+            {site}
+            <button
+              onClick={() =>
+                onEditWebsite(siteIndex, prompt("Edit website", site))
+              }
+            >
+              Edit
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 BlockedSitesSection.propTypes = {
+  index: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   sites: PropTypes.array.isRequired,
-  onAddSite: PropTypes.func.isRequired,
-  onEditSite: PropTypes.func.isRequired,
-  onDeleteSite: PropTypes.func.isRequired,
+  isModalOpen: PropTypes.bool.isRequired,
+  onOpenModal: PropTypes.func.isRequired,
+  onCloseModal: PropTypes.func.isRequired,
+  onAddWebsite: PropTypes.func.isRequired,
+  onEditWebsite: PropTypes.func.isRequired,
+  onEditSectionTitle: PropTypes.func.isRequired,
 };
--- src/components/WebsiteBlocker.jsx
+++ src/components/WebsiteBlocker.jsx
@@ -1,68 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import BlockedSitesSection from "./BlockedSitesSection";
-import AddBlockedSiteModal from "./AddBlockedSiteModal";
 
 export default function WebsiteBlocker() {
   const [sections, setSections] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isAddingSection, setIsAddingSection] = useState(false);
-  const [editIndex, setEditIndex] = useState(null);
-  const [currentEditSite, setCurrentEditSite] = useState(null);
-  const [currentEditSectionIndex, setCurrentEditSectionIndex] = useState(null);
+  const [activeModalIndex, setActiveModalIndex] = useState(null);
 
-  function addSiteToSection(site) {
-    console.log("function add site called");
+  const addSection = (title) => {
+    setSections([...sections, { title, sites: [] }]);
+  };
+
+  const editSectionTitle = (index, newTitle) => {
     const updatedSections = [...sections];
-    if (isAddingSection) {
-      if (site.trim() !== "") {
-        setSections([...sections, { title: site, sites: [] }]);
-      }
-    } else if (site && currentEditSectionIndex !== null) {
-      if (editIndex !== null) {
-        updatedSections[currentEditSectionIndex].sites[editIndex] = site;
-      } else {
-        updatedSections[currentEditSectionIndex].sites.push(site);
-      }
-      console.log(updatedSections);
-      setSections(updatedSections);
-      resetModalAndEditState();
-    }
-  }
-
-  useEffect(() => {
-    console.log("Sections updated", sections);
-  }, [sections]);
-
-  function openModalToAddNewSection() {
-    setIsAddingSection(true);
-    setIsModalOpen(true);
-    resetEditStates();
-  }
-
-  function openModalForEdit(sectionIndex, siteIndex) {
-    setIsAddingSection(false);
-    setIsModalOpen(true);
-    setEditIndex(siteIndex);
-    setCurrentEditSite(sections[sectionIndex].sites[siteIndex]);
-    setCurrentEditSectionIndex(sectionIndex);
-  }
-
-  function resetModalAndEditState() {
-    setIsModalOpen(false);
-    resetEditStates();
-  }
-
-  function resetEditStates() {
-    setEditIndex(null);
-    setCurrentEditSite(null);
-    setCurrentEditSectionIndex(null);
-  }
-
-  function deleteSite(sectionIndex, siteIndex) {
-    const updatedSections = [...sections];
-    updatedSections[sectionIndex].sites.splice(siteIndex, 1);
+    updatedSections[index].title = newTitle;
     setSections(updatedSections);
-  }
+  };
+
+  const addWebsiteToSection = (sectionIndex, website) => {
+    const updatedSections = [...sections];
+    updatedSections[sectionIndex].sites.push(website);
+    setSections(updatedSections);
+  };
+
+  const editWebsiteInSection = (sectionIndex, websiteIndex, newWebsite) => {
+    const updatedSections = [...sections];
+    updatedSections[sectionIndex].sites[websiteIndex] = newWebsite;
+    setSections(updatedSections);
+  };
+
+  const openModal = (index) => setActiveModalIndex(index);
+  const closeModal = () => setActiveModalIndex(null);
 
   return (
     <div className="blocker-container">
@@ -70,27 +36,22 @@ export default function WebsiteBlocker() {
       {sections.map((section, index) => (
         <BlockedSitesSection
           key={index}
+          index={index}
           title={section.title}
           sites={section.sites}
-          onAddSite={() => {
-            setIsAddingSection(false);
-            setCurrentEditSectionIndex(index);
-            setIsModalOpen(true);
-          }}
-          onEditSite={(siteIndex) => openModalForEdit(index, siteIndex)}
-          onDeleteSite={(siteIndex) => deleteSite(index, siteIndex)}
+          isModalOpen={activeModalIndex === index}
+          onOpenModal={() => openModal(index)}
+          onCloseModal={closeModal}
+          onAddWebsite={(website) => addWebsiteToSection(index, website)}
+          onEditWebsite={(websiteIndex, newWebsite) =>
+            editWebsiteInSection(index, websiteIndex, newWebsite)
+          }
+          onEditSectionTitle={(newTitle) => editSectionTitle(index, newTitle)}
         />
       ))}
-
-      <AddBlockedSiteModal
-        isOpen={isModalOpen}
-        onClose={resetModalAndEditState}
-        onAddOrEditSite={addSiteToSection}
-        currentEditSite={currentEditSite}
-        isAddingSection={isAddingSection}
-      />
-
-      <button onClick={openModalToAddNewSection}>Add Section</button>
+      <button onClick={() => addSection(`Section${sections.length + 1}`)}>
+        Add Section
+      </button>
     </div>
   );
 }
